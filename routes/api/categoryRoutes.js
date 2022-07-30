533d8f355138e1297ed5ec51d6eb033e1b8278b6
Mackenzie <mackenziejoyce414@gmail.com>
2022-07-30 16:17:00 -0400

    add router to update category by id

--- routes/api/categoryRoutes.js
+++ routes/api/categoryRoutes.js
@@ -58,6 +58,23 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try { 
+    const categoryUpdate = await Category.update(req.params.id, { include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+    })
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id' })
+      return
+    }
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 })
 
 router.delete('/:id', async (req, res) => {
