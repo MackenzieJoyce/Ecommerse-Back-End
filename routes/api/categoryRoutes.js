da9e0f5dc84e7fd1aa1ccaf54cf7136b007f57a4
Mackenzie <mackenziejoyce414@gmail.com>
2022-07-30 15:22:37 -0400

    created router for category find by id using same structure as above. Going to use that same similar method for the create, update, and delete routers. Using the orm mini project as reference for these.

--- routes/api/categoryRoutes.js
+++ routes/api/categoryRoutes.js
@@ -1,5 +1,5 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require('express').Router()
+const { Category, Product } = require('../../models')
 
 // The `/api/categories` endpoint
 
@@ -7,42 +7,50 @@ router.get('/', async (req, res) => {
   // find all categories
   try {
     const categoryData = await Category.findAll({
-      include: [{
-        model: Product,
-        // be sure to include its associated Products
-        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
-      }]
+      include: [
+        {
+          model: Product,
+          // be sure to include its associated Products
+          attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        }
+      ]
     })
 
     res.status(200).json(categoryData)
   } catch (err) {
     res.status(500).json(err)
   }
-});
+})
 
 router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   try {
-    const categoryFind = await Category.findByPk({ 
-      include: [{
-        model: Product,
-        // be sure to include its associated Products
-        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
-      }]
+    const categoryFind = await Category.findByPk(req.params.id, {
+      include: [
+        {
+          model: Product,
+          // be sure to include its associated Products
+          attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        }
+      ]
     })
-});
-});
+
+    res.status(200).json(categoryFind).json(categoryFind)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
 
 router.post('/', (req, res) => {
   // create a new category
-});
+})
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-});
+})
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
-});
+})
 
-module.exports = router;
+module.exports = router
