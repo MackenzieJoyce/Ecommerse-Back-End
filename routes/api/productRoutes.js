4dde15ca999249812ee36d21bb804ac2fcc41a59
Mackenzie <mackenziejoyce414@gmail.com>
2022-07-31 13:27:48 -0400

    fixed up router for product

--- routes/api/productRoutes.js
+++ routes/api/productRoutes.js
@@ -50,7 +50,7 @@ router.get('/:id', async (req, res) => {
       return
     }
 
-    res.status(200).json(productFind).json(productFind)
+    res.status(200).json(productFind)
   } catch (err) {
     res.status(500).json(err)
   }
